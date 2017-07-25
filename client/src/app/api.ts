76bea70bbdbcc9ba672c675c361764fca66d4c9a
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-07-25 19:33:36 +0200

    Init

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -1,7 +1,7 @@
 export class Api {
 
-  baseUrl = 'http://localhost:8080/api-v1'
-  oauthServer = 'http://localhost:8080/oauth/token'
+  baseUrl = 'https://localhost:8080/api-v1'
+  oauthServer = 'https://localhost:8080/oauth/token'
   activeUser = `${this.baseUrl}/users/active`
 
 }
