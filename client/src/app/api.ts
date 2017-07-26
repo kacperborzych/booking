1af8d5f673a9e9cd95d18d4e640dad7f48f10e3b
kacperborzych@o2.pl <kacperborzych@users.noreply.github.com>
2017-07-26 14:55:41 +0200

    made angular for spring

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -3,5 +3,6 @@ export class Api {
   baseUrl = 'https://localhost:8080/api-v1'
   oauthServer = 'https://localhost:8080/oauth/token'
   activeUser = `${this.baseUrl}/users/active`
+  hotels = `${this.baseUrl}/hotels`
 
 }
